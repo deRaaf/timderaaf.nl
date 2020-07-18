60ad38a408391959b45559de03ce27a9af483687
Tim de Raaf <timraaf@hotmail.com>
2020-07-19 00:15:49 +0200

    ini & hero (desktop)

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './scss/settings.scss';
+
+// html,
+// body,
+// #app {
+//   height: 100%;
+//   width: 100%;
+//   max-width: 100%;
+//   max-height: 100%;
+// }
+
+#app {
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <hero/>
+  </div>
+</template>
+
+<script>
+import hero from '@/components/hero.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    hero,
+  }
+}
+</script>
+
+<style lang="scss">
+  .home {
+    height: 100%;
+    width: 100%;
+  }
+</style>
